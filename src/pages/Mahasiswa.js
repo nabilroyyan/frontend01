295404895de4976d9375e644822969ce3a07202f
nabilroyyan <nabil.royyan1203@gmail.com>
2023-10-17 16:38:26 +0700

    praktikum 19

---
+++ src/pages/Mahasiswa.js
@@ -0,0 +1,18 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+function mahasiswa() {
+
+
+  
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2>data mahasiswa</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default mahasiswa;
